4b61c375e1ae03d3b2834bba2b96359a0ed7a84e
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-02-27 16:54:40 +0530

    Add logic to choose closest lift

--- lift/views.py
+++ lift/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from .models import Lift
+import sys
 
 # Create your views here.
 def create_lift(request):
@@ -16,7 +17,8 @@ def list_lift(request):
     return JsonResponse(lift_object)
 
 def move_lift(request):
-    return JsonResponse({"Moved lift": 1})
+    closest_Lift = choose_lift(5)
+    return JsonResponse({"Moved lift": closest_Lift})
 
 def remove_lift(request):
     lift = Lift.objects.last()
@@ -25,3 +27,14 @@ def remove_lift(request):
         lift.delete()
         return JsonResponse({"Removed lift": deleted_lift})
     return JsonResponse({"Error": "Cant remove a moving lift"})
+
+
+def choose_lift(requiredFloor):
+    lifts = Lift.objects.all()
+    distance = abs(requiredFloor-lifts[0].current_floor)
+    closest_lift = 1
+    for lift in lifts:
+        if abs(requiredFloor-lift.current_floor) < distance:
+            distance = abs(requiredFloor-lift.current_floor)
+            closest_lift = lift.id
+    return closest_lift
